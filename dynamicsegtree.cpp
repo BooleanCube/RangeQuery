6106b624b426fa68151ec9ff2332e239a622d4a0
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2022-01-26 20:53:54 -0500

    Update dynamicsegtree.cpp

--- dynamicsegtree.cpp
+++ dynamicsegtree.cpp
@@ -1,47 +1,45 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <stdint.h>
 using namespace std;
 
-void constructTree(int nums[], int  a) {
-	for(int i=a; i<=2*a; i++) {
-		int b = i;
+void constructTree(vector<uint64_t>* nums, uint64_t a) {
+	for(uint64_t i=a; i<=2*a; i++) {
+		uint64_t b = i;
 		while(b>1) {
 			b>>=1;
-			nums[b] += nums[i];
-			cout << b << " " << nums[b] << endl;
+			nums->at(b) += nums->at(i);
 		}
 	}
 }
 
-void updateTree(int nums[], int k, int w) {
-	int a = sizeof(nums)/sizeof(nums[0])/2;
-	int b = a+k-1;
-	int v = nums[b];
-	nums[b] = w;
+void updateNode(vector<uint64_t>* nums, uint64_t k, uint64_t w) {
+	uint64_t v = nums->at(k);
+  uint64_t b = k;
+	nums->at(k) = w;
 	while(b>1) {
 		b>>=1;
-		nums[b] -= v;
-		nums[b] += w;
+		nums->at(b) -= v;
+		nums->at(b) += w;
 	}
 }
 
-int getSumRange(int nums[], int k, int w, int sum) {
-	if(k == w) return nums[k] + sum;
-	if(k%2 == 1) { sum += nums[k]; k++; }
-	if(w%2 == 0) { sum += nums[w]; w--; }
-	return getSumRange(nums, k>>1, w>>1, sum);
+uint64_t getSumRange(vector<uint64_t>* nums, uint64_t k, uint64_t w, uint64_t sum) {
+  if(k == w) return nums->at(k) + sum;
+  if(k%2 == 1) { sum += nums->at(k); k++; }
+  if(k == w) return nums->at(k) + sum;
+  if(w%2 == 0) { sum += nums->at(w); w--; }
+  return getSumRange(nums, k>>1, w>>1, sum);
 }
 
 int main() {
-	int a, b; cin >> a >> b;
-	int nums[2*a+1];
-	for(int i=0; i<a; i++) cin >> nums[a+i];
-	cout << endl;
+	uint64_t a, b; cin >> a >> b;
+	vector<uint64_t>* nums = new vector<uint64_t>(2*a+1, 0);
+	for(uint64_t i=0; i<a; i++) cin >> nums->at(a+i);
 	constructTree(nums, a);
-	cout << "yes";
-	for(int i=0; i<b; i++) {
-		int q, k, w; cin >> q >> k >> w;
-		cout << "yes";
-		if(q == 1) updateTree(nums, k, w);
-		else cout << getSumRange(nums, k, w, 0) << endl;
+	for(uint64_t i=0; i<b; i++) {
+		uint64_t q, k, w; cin >> q >> k >> w;
+		if(q == 1) updateNode(nums, a+k-1, w);
+		else cout << getSumRange(nums, a+k-1, a+w-1, 0) << endl;
 	}
 }
